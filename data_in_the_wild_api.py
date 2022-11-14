0a19f429037c1d3aedc201c6109e986f774e20e1
Harpa Guðrún Hreinsdóttir <harpa@expectus.is>
2022-11-14 14:13:52 +0100

    adding more methods and list of handles

--- data_in_the_wild_api.py
+++ data_in_the_wild_api.py
@@ -21,26 +21,17 @@ def get_influencers_user_ids(influencers):
     influencers_ids = []
     return influencers_ids
 
-def get_user_posts(influencer):
+def get_user_posts(influencer_id):
     user_posts = []
+    is_finished = false
+    end_cursor = ""
+    while is_finished == false:
+        posts, is_finished, end_cursor = get_user_posts_from_api(end_cursor, user_id)
+        user_posts.append(posts)
     return user_posts
 
-def add_posts_to_dataframe():
-    posts_dataframe = []
-    return posts_dataframe
-
-
-def main():
-    influencers_handles = []
-
-    influencers_ids = get_influencers_user_ids(influencers_handles)
-
-    for influencer in influencers_ids:
-        user_posts = get_user_posts(influencer)
-        add_posts_to_dataframe(user_posts)
-
-    #JUST FOR TESTING
-    url = "https://instagram188.p.rapidapi.com/userpost/25025320/12/%7Bend_cursor%7D"
+def get_user_posts_from_api(end_cursor, user_id):
+    url = "https://instagram188.p.rapidapi.com/userpost/25025320/50/%7Bend_cursor%7D"
 
     headers = {
         "X-RapidAPI-Key": "6b130ce03bmshe9b1c5345b5fd64p151030jsn429f30217f89",
@@ -48,8 +39,30 @@ def main():
     }
 
     response = requests.request("GET", url, headers=headers)
+    posts = retrieve_information_needed(response.text)
+    return end_date, posts
 
-    print(response.text)
+def retrieve_information_needed(response_text):
+    posts = []
+    # appending instances to list
+    posts.append(instagram_post(url, date_time, caption, likes, user_id, insta_handle, post_id))
+    return posts
+
+def add_posts_to_dataframe(posts):
+    posts_dataframe = []
+    return posts_dataframe
+
+
+def main():
+    influencers_handles = ['herdisathena', 'mathilde_roien', 'Josefinehj', 'Birtahlin', 'Katarinakrebs', 'Kristinetrinkjaer',
+                        'Chloemonchamp', 'Emiliemalou', 'Emmamoldt', 'Barbaraegholm', 'Annasarlvit' 'Filippajuhler', 'Karla_alajdi',
+                         'Ellakarberg', 'Annakatrinkafehr', 'Annabjorkjohansson', 'Pernilleteisbaek', 'Emilisindlev', 'Madsdamind',
+                         'Simonenoa', 'Mathildegoehler', 'Sarahdahll', 'Tommyleewinkworth', 'kennethnguyen']
+    influencers_ids = get_influencers_user_ids(influencers_handles)
+    for influencer in influencers_ids:
+        user_posts = get_user_posts(influencer)   #user_posts is of type List<instagram_post>
+        add_posts_to_dataframe(user_posts)
+
 
 if __name__ == "__main__":
     main()
