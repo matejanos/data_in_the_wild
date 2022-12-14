8661629616c3d818d429a9e85f0dcdd79454ce6d
matejanos <m.gunda98@gmail.com>
2022-12-14 08:49:43 +0100

    download the images from URLs

--- image_from_csv.py
+++ image_from_csv.py
@@ -4,21 +4,22 @@ import os
 import urllib.request
 
 
-posts = pd.read_csv('test.csv')
+posts = pd.read_csv('data_final_2.csv')
 links = posts.url
 #print(links)
 
-dir = 'images'
+dir = 'images2'
 
 path = os.getcwd()
 path = os.path.join(path, dir)
 os.mkdir(path)
 
 for index, row in posts.iterrows():
-    #print(row['url'], row['user_id'])
-    filename = f"{row['user_id']}.jpg"
+    print(row['url'], row['unique_id'])
+    filename = f"{row['unique_id']}.jpg"
     urllib.request.urlretrieve(row['url'], f"{path}/{filename}")
     print(filename)
+    print(index)
 
 # f = open('test2.jpg','wb')
 # f.write(requests.get('https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Neymar_PSG.jpg/1200px-Neymar_PSG.jpg').content)
